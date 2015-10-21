29db71bd042dbad8342b17c9005101c385109de1
Peter Barr <peter@localhost.localdomain>
2015-10-21 01:06:54 -0400

    Minor

--- R/RcppExports.R
+++ R/RcppExports.R
@@ -17,15 +17,3 @@ subsets_shortest_paths_c <- function(nv, el, s) {
     .Call('gclust_subsets_shortest_paths_c', PACKAGE = 'gclust', nv, el, s)
 }
 
-get_thread_info <- function() {
-    invisible(.Call('gclust_get_thread_info', PACKAGE = 'gclust'))
-}
-
-sqrt_many <- function(v) {
-    .Call('gclust_sqrt_many', PACKAGE = 'gclust', v)
-}
-
-sqrt_many_omp <- function(v) {
-    .Call('gclust_sqrt_many_omp', PACKAGE = 'gclust', v)
-}
-
