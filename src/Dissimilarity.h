dca9f0821426cf2ea07a3c94cd7f56787afee450
Peter Barr <barrpet@gmail.com>
2015-10-27 07:14:53 -0400

    Updated Dissimilarity to use gclust_types

--- src/Dissimilarity.h
+++ src/Dissimilarity.h
@@ -9,22 +9,23 @@ Rcpp::NumericVector dissimilarity_sp_c(const AdjacencyArray& adj,
   const gclust::MatrixUS& sp);
 
 // [[Rcpp::export(rng = false)]]
-Rcpp::NumericVector dissimilarity_sp_c(long nv, const Rcpp::IntegerMatrix& el,
-  const gclust::MatrixUS& sp);
+Rcpp::NumericVector dissimilarity_sp_c(const gclust::idx_t nv,
+  const Rcpp::IntegerMatrix& el, const gclust::MatrixUS& sp);
 
 // [[Rcpp::export(rng = false)]]
-Rcpp::NumericVector dissimilarity_c(long nv, const Rcpp::IntegerMatrix& el);
+Rcpp::NumericVector dissimilarity_c(const gclust::idx_t nv,
+  const Rcpp::IntegerMatrix& el);
 
 Rcpp::NumericVector dissimilarity_subsets_sp_c(const AdjacencyArray& adj, const
   Rcpp::IntegerVector& s, const gclust::MatrixUS& sp);
 
 // [[Rcpp::export(rng = false)]]
-Rcpp::NumericVector dissimilarity_subsets_sp_c(int nv, const
-  Rcpp::IntegerMatrix& el, const Rcpp::IntegerVector& s, const gclust::MatrixUS&
-  sp);
+Rcpp::NumericVector dissimilarity_subsets_sp_c(const gclust::idx_t nv,
+  const Rcpp::IntegerMatrix& el, const Rcpp::IntegerVector& s,
+  const gclust::MatrixUS& sp);
 
 // [[Rcpp::export(rng = false)]]
-Rcpp::NumericVector dissimilarity_subsets_c(int nv, const
-  Rcpp::IntegerMatrix& el, const Rcpp::IntegerVector& s);
+Rcpp::NumericVector dissimilarity_subsets_c(const gclust::idx_t nv,
+  const Rcpp::IntegerMatrix& el, const Rcpp::IntegerVector& s);
 
 #endif //GCLUST_DISSIMILARITY_H
