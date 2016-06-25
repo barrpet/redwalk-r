2d5d8f0d31c6a8f49379298f881f0418178d5d59
Peter Barr <barrpet@gmail.com>
2016-06-25 01:14:14 -0400

    completed dissimilarity for full graph case

--- R/Dissimilarity.R
+++ R/Dissimilarity.R
@@ -34,21 +34,21 @@ dissimilarity <- function(graph, nodes = V(graph), short_paths = NULL)
 
   # get shortest paths if needed, or check provided
   if(is.null(short_paths)) {
-    short_paths = as.dist(shortest_path_lengths(graph, nodes))
+    short_paths = shortest_path_lengths(graph, nodes)
   } else {
     # check short_paths arg
     if("dist" %in% class(short_paths)) {
       # check correct size
       nvd <- attr(short_paths, "Size")
       stopifnot(attr(short_paths, "Size") == ns)
+      # convert to matrix
+      short_paths = as.matrix(short_paths)
     } else if("matrix" %in% class(short_paths)) {
       # check square and correct size
       nm = dim(short_paths)
       stopifnot(nm[0] == nm[1], nm[0] == ns)
       # check symmetric
-      stopifnot(all(short_pahts == t(short_paths)))
-      # convert to dist object
-      short_paths = as.dist(short_paths)
+      stopifnot(all(short_paths == t(short_paths)))
     } else {
       # not a matrix or dist, error
       # TODO: might be coercible to matrix
@@ -66,11 +66,25 @@ dissimilarity <- function(graph, nodes = V(graph), short_paths = NULL)
 # compute the dissimilarity for all the nodes in the graph
 dissimilarity_full <- function(graph, short_paths)
 {
-  stop("Not yet implemented")
+  # for all nodes in the graph, D_ij is simply the average of the shortest paths
+  # of the neighbors of i to j.
+  # To make symmetric, D_ij = D_ji = min(D_ij, D_ji)
+  # Can do adjacency matrix times shortest paths matrix then divide rows by the
+  # degree of the node
+  A <- as_adj(graph, type = "both", names = FALSE)
+  deg <- degree(graph, mode = "out", loops = FALSE)
+  D <- as.matrix(A %*% short_paths / deg)
+  return(as.dist(pmin(D, t(D))))
 }
 
 # compute the dissimilarity for a subset of the nodes in the graph
 dissimilarity_subsets <- function(graph, nodes, short_paths)
 {
   stop("Not yet implemented")
+  # 1) create random walk normalized laplacian
+  # 2) factor the above matrix
+  # 3) create B matrix (columns of b vectors)
+  # 4) Solve
+  # 5) Do average of neighbors, making symmetric
+  # 6) Convert to dist object and return
 }
