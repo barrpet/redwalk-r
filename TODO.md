439cc5caaafee2daea2ed07f2bac04a0a67d6fc3
Peter Barr <barrpet@gmail.com>
2016-06-25 09:09:57 -0400

    clean up benchmarks

--- TODO.md
+++ TODO.md
@@ -1,11 +1,8 @@
 # TODO
 
-- Need to import Rcpp, but check complains if not required?
-- Fails for WIN32
-- Merge subsets and full functions
-- Make use of OpenMP where applicable
 - Be able to return igraph "communities" rather than just hclust
 - Clean up documentation
 - Improve argument validation
 - Add more comprehensive test coverage
 - Add first NEWS entry
+- Set up better benchmarks
---
+++ inst/benchmarks/time_cluster_comparison.R
@@ -0,0 +1,28 @@
+library('rbenchmark', quietly = TRUE, warn.conflicts = FALSE)
+library('redwalk', quietly = TRUE, warn.conflicts = FALSE)
+
+comparison_benchmark <- function(graph_name, reps = 5) {
+  g <- get(as.character(graph_name))
+  sp <- shortest_path_lengths(g)
+
+  res <- rbenchmark::benchmark(cluster_redwalk(g),
+                               cluster_redwalk(g, short_paths = sp),
+                               cluster_walktrap(g, steps = 3),
+                               cluster_walktrap(g, steps = 4),
+                               cluster_walktrap(g, steps = 5),
+                               cluster_walktrap(g, steps = 6),
+                               cluster_leading_eigen(g),
+                               cluster_fast_greedy(g),
+                               order = "elapsed", replications = reps)
+
+  print(paste("+++ ", graph_name, " +++"))
+  print(res)
+  print('')
+}
+
+comparison_benchmark('dolphins', 100)
+comparison_benchmark('polbooks', 50)
+comparison_benchmark('polblogs', 5)
+comparison_benchmark('power', 2)
+
+unloadNamespace('rbenchmark')
